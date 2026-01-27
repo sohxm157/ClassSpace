817b4b2f5efce04882ccf2712d149f8f57aec577
afrid341 <67476611+afrid341@users.noreply.github.com>
2026-01-27 20:11:07 +0530

    Add new backend project folder

---
+++ backend/ClassSpace/src/main/java/com/cdac/ClassSpace/ClassSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.cdac.ClassSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassSpaceApplication.class, args);
+	}
+
+}
